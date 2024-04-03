2923c1ccb45da5511b3eb27b5c02064dab093047
xogns3725 <xogns3725@naver.com>
2024-04-03 14:38:00 +0900

    에러 수정

--- post/urls.py
+++ post/urls.py
@@ -6,7 +6,7 @@ app_name = "post"
 urlpatterns = [
     path("",views.index, name='index'),
     path("<int:pk>/", views.post_detail, name="post_detail"),
-    path("register/", views.post_register, name="post_register"),
+    path("register/index.html", views.post_register, name="post_register"),
     path('<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('<int:pk>/delete/', views.post_delete, name='post_delete'),
 ]
