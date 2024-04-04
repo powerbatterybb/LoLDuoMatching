7c1b079aa7cb1f7042515c15648018328d45d74d
BeomJung <kbeom0124@naver.com>
2024-04-04 19:11:41 +0900

    signup html

--- post/forms.py
+++ post/forms.py
@@ -3,10 +3,11 @@ from .models import Post, Comment
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class  PostForm(forms.ModelForm):
-  class Meta:
-    model = Post
-    fields = ('title','contents')
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'contents')
 
 
 class CommentForm(forms.ModelForm):
@@ -14,9 +15,14 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ['author', 'content']
 
+
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    email = forms.EmailField(
+        max_length=254, help_text='Required. Inform a valid email address.')
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        labels = {
+            'username': 'ID'
+        }
